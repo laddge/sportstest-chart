be782cfcd1339616172b5120fefc52adbfd15a0c
Laddge <dev.laddge@gmail.com>
2024-09-14 23:10:05 +0900

    feat: 表の表示を実装

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,118 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useRecordStore } from './stores/recordStore'
+const {
+    gender,
+    result,
+    resultScores,
+    uecAve,
+    uecAveScores,
+    jpnAve,
+    jpnAveScores
+  } = storeToRefs(useRecordStore())
+const age = ref(18)
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="max-w-[1024px] mx-auto p-6 print:p-8 print:text-xs">
+    <div class="flex items-center">
+      <div>
+        4月1日時点での年齢
+        <input type="number" v-model="age" class="border w-20 text-right mx-1 print:hidden" />
+        <span class="hidden print:inline-block w-8 text-right">{{ age }}</span>
+        歳
+      </div>
+      <div class="ml-4 flex items-center">
+        性別
+        <div class="print:hidden ml-2 flex items-center">
+          <label class="cursor-pointer flex items-center">
+            <input type="radio" value="m" v-model="gender" class="mr-1" />
+            男
+          </label>
+          <label class="cursor-pointer flex items-center ml-2">
+            <input type="radio" value="f" v-model="gender" class="mr-1" />
+            女
+          </label>
+        </div>
+        <div class="hidden print:flex items-center ml-2">
+          (
+          <span :class="gender == 'm' ? 'inline-block w-[1.5em] h-[1.5em] text-center border border-black rounded-full' : ''">男</span>
+          ・
+          <span :class="gender == 'f' ? 'inline-block w-[1.5em] h-[1.5em] text-center border border-black rounded-full' : ''">女</span>
+          )
+        </div>
+      </div>
+    </div>
+    <div  v-if="!gender" class="text-red-900 bg-red-100 font-bold p-2 rounded-sm mt-2">
+      性別を選択してください
+    </div>
+    <div class="overflow-auto mt-4">
+      <table class="border-y border-black w-full">
+        <caption class="mb-2">
+          表1: 体力テストの結果
+        </caption>
+        <thead>
+          <tr class="border-y border-black">
+            <th></th>
+            <th></th>
+            <th class="p-1">握力<br />(kg)</th>
+            <th class="p-1">上体起こし<br />(回)</th>
+            <th class="p-1">長座体前屈<br />(cm)</th>
+            <th class="p-1">反復横とび<br />(点)</th>
+            <th class="p-1">持久走<br />(分:秒)</th>
+            <th class="p-1">50m走<br />(秒)</th>
+            <th class="p-1">立ち幅とび<br />(cm)</th>
+            <th class="p-1">ﾊﾝﾄﾞﾎﾞｰﾙ投げ<br />(m)</th>
+            <th class="p-1">総合点<br />(点)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th rowspan="2" class="text-nowrap p-2">今回の結果</th>
+            <th class="text-nowrap p-1">記録</th>
+            <td v-for="n in 9" :key="n" class="px-1 text-center">
+              <input v-model="result[n - 1]" class="w-full min-w-10 text-center border print:hidden" :disabled="!gender" />
+              <span class="hidden print:inline">{{ result[n - 1] }}</span>
+            </td>
+          </tr>
+          <tr>
+            <th class="text-nowrap p-1">判定</th>
+            <td v-for="n in 9" :key="n" class="text-center">{{ resultScores[n - 1] }}</td>
+          </tr>
+          <tr>
+            <th rowspan="2" class="text-nowrap p-2">電通大平均</th>
+            <th class="text-nowrap p-1">記録</th>
+            <td v-for="n in 9" :key="n" class="px-1 text-center">
+              <input v-model="uecAve[n - 1]" class="w-full text-center border print:hidden" :disabled="!gender" />
+              <span class="hidden print:inline">{{ uecAve[n - 1] }}</span>
+            </td>
+          </tr>
+          <tr>
+            <th class="text-nowrap p-1">判定</th>
+            <td v-for="n in 9" :key="n" class="text-center">{{ uecAveScores[n - 1] }}</td>
+          </tr>
+          <tr>
+            <th rowspan="2" class="text-nowrap p-2">全国平均</th>
+            <th class="text-nowrap p-1">記録</th>
+            <td v-for="n in 9" :key="n" class="px-1 text-center">
+              <input v-model="jpnAve[n - 1]" class="w-full text-center border print:hidden" :disabled="!gender" />
+              <span class="hidden print:inline">{{ jpnAve[n - 1] }}</span>
+            </td>
+          </tr>
+          <tr>
+            <th class="text-nowrap p-1">判定</th>
+            <td v-for="n in 9" :key="n" class="text-center">{{ jpnAveScores[n - 1] }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+  @page {
+    margin: 0;
+    size: A4 portrait;
+  }
 </style>
---
+++ src/stores/recordStore.ts
@@ -0,0 +1,247 @@
+import { ref, computed, Ref } from 'vue'
+import { defineStore } from 'pinia'
+
+type Array9<T> = [T, T, T, T, T, T, T, T, T]
+type Records = Array9<string>
+
+const computeScores = (gender: Ref<'m'|'f'|null>, records: Ref<Records>) => {
+  const scores: Records = ['', '', '', '', '', '', '', '', '']
+  if (gender.value == null) return scores
+
+  // 握力
+  if (/^\d+(\.\d+)?$/.test(records.value[0])) {
+    const record = Number(records.value[0])
+    if (record >= { m: 56, f: 36 }[gender.value]) {
+      scores[0] = '10'
+    } else if (record >= { m: 51, f: 33 }[gender.value]) {
+      scores[0] = '9'
+    } else if (record >= { m: 47, f: 30 }[gender.value]) {
+      scores[0] = '8'
+    } else if (record >= { m: 43, f: 28 }[gender.value]) {
+      scores[0] = '7'
+    } else if (record >= { m: 38, f: 25 }[gender.value]) {
+      scores[0] = '6'
+    } else if (record >= { m: 33, f: 23 }[gender.value]) {
+      scores[0] = '5'
+    } else if (record >= { m: 28, f: 20 }[gender.value]) {
+      scores[0] = '4'
+    } else if (record >= { m: 23, f: 17 }[gender.value]) {
+      scores[0] = '3'
+    } else if (record >= { m: 18, f: 14 }[gender.value]) {
+      scores[0] = '2'
+    } else {
+      scores[0] = '1'
+    }
+  }
+
+  // 上体起こし
+  if (/^\d+(\.\d+)?$/.test(records.value[1])) {
+    const record = Number(records.value[1])
+    if (record >= { m: 35, f: 29 }[gender.value]) {
+      scores[1] = '10'
+    } else if (record >= { m: 33, f: 26 }[gender.value]) {
+      scores[1] = '9'
+    } else if (record >= { m: 30, f: 23 }[gender.value]) {
+      scores[1] = '8'
+    } else if (record >= { m: 27, f: 20 }[gender.value]) {
+      scores[1] = '7'
+    } else if (record >= { m: 25, f: 18 }[gender.value]) {
+      scores[1] = '6'
+    } else if (record >= { m: 22, f: 15 }[gender.value]) {
+      scores[1] = '5'
+    } else if (record >= { m: 19, f: 13 }[gender.value]) {
+      scores[1] = '4'
+    } else if (record >= { m: 16, f: 11 }[gender.value]) {
+      scores[1] = '3'
+    } else if (record >= { m: 13, f: 8 }[gender.value]) {
+      scores[1] = '2'
+    } else {
+      scores[1] = '1'
+    }
+  }
+
+  // 長座体前屈
+  if (/^\d+(\.\d+)?$/.test(records.value[2])) {
+    const record = Number(records.value[2])
+    if (record >= { m: 64, f: 63 }[gender.value]) {
+      scores[2] = '10'
+    } else if (record >= { m: 58, f: 58 }[gender.value]) {
+      scores[2] = '9'
+    } else if (record >= { m: 53, f: 54 }[gender.value]) {
+      scores[2] = '8'
+    } else if (record >= { m: 49, f: 50 }[gender.value]) {
+      scores[2] = '7'
+    } else if (record >= { m: 44, f: 45 }[gender.value]) {
+      scores[2] = '6'
+    } else if (record >= { m: 39, f: 40 }[gender.value]) {
+      scores[2] = '5'
+    } else if (record >= { m: 33, f: 35 }[gender.value]) {
+      scores[2] = '4'
+    } else if (record >= { m: 28, f: 30 }[gender.value]) {
+      scores[2] = '3'
+    } else if (record >= { m: 21, f: 22 }[gender.value]) {
+      scores[2] = '2'
+    } else {
+      scores[2] = '1'
+    }
+  }
+
+  // 反復横とび
+  if (/^\d+(\.\d+)?$/.test(records.value[3])) {
+    const record = Number(records.value[3])
+    if (record >= { m: 63, f: 53 }[gender.value]) {
+      scores[3] = '10'
+    } else if (record >= { m: 60, f: 50 }[gender.value]) {
+      scores[3] = '9'
+    } else if (record >= { m: 56, f: 48 }[gender.value]) {
+      scores[3] = '8'
+    } else if (record >= { m: 53, f: 45 }[gender.value]) {
+      scores[3] = '7'
+    } else if (record >= { m: 49, f: 42 }[gender.value]) {
+      scores[3] = '6'
+    } else if (record >= { m: 45, f: 39 }[gender.value]) {
+      scores[3] = '5'
+    } else if (record >= { m: 41, f: 36 }[gender.value]) {
+      scores[3] = '4'
+    } else if (record >= { m: 37, f: 32 }[gender.value]) {
+      scores[3] = '3'
+    } else if (record >= { m: 30, f: 27 }[gender.value]) {
+      scores[3] = '2'
+    } else {
+      scores[3] = '1'
+    }
+  }
+
+  // 持久走
+  if (/^\d+:\d+$/.test(records.value[4])) {
+    const record = Number(records.value[4].split(':')[0]) * 60 + Number(records.value[4].split(':')[1])
+    if (record <= { m: 299, f: 229 }[gender.value]) {
+      scores[4] = '10'
+    } else if (record <= { m: 316, f: 242 }[gender.value]) {
+      scores[4] = '9'
+    } else if (record <= { m: 333, f: 259 }[gender.value]) {
+      scores[4] = '8'
+    } else if (record <= { m: 355, f: 277 }[gender.value]) {
+      scores[4] = '7'
+    } else if (record <= { m: 382, f: 296 }[gender.value]) {
+      scores[4] = '6'
+    } else if (record <= { m: 410, f: 318 }[gender.value]) {
+      scores[4] = '5'
+    } else if (record <= { m: 450, f: 342 }[gender.value]) {
+      scores[4] = '4'
+    } else if (record <= { m: 499, f: 374 }[gender.value]) {
+      scores[4] = '3'
+    } else if (record <= { m: 560, f: 417 }[gender.value]) {
+      scores[4] = '2'
+    } else {
+      scores[4] = '1'
+    }
+  }
+
+  // 50m走
+  if (/^\d+\.\d+$/.test(records.value[5])) {
+    const record = Number(records.value[5])
+    if (record <= { m: 6.6, f: 7.7 }[gender.value]) {
+      scores[5] = '10'
+    } else if (record <= { m: 6.8, f: 8.0 }[gender.value]) {
+      scores[5] = '9'
+    } else if (record <= { m: 7.0, f: 8.3 }[gender.value]) {
+      scores[5] = '8'
+    } else if (record <= { m: 7.2, f: 8.6 }[gender.value]) {
+      scores[5] = '7'
+    } else if (record <= { m: 7.5, f: 8.9 }[gender.value]) {
+      scores[5] = '6'
+    } else if (record <= { m: 7.9, f: 9.3 }[gender.value]) {
+      scores[5] = '5'
+    } else if (record <= { m: 8.4, f: 9.8 }[gender.value]) {
+      scores[5] = '4'
+    } else if (record <= { m: 9.0, f: 10.3 }[gender.value]) {
+      scores[5] = '3'
+    } else if (record <= { m: 9.7, f: 11.2 }[gender.value]) {
+      scores[5] = '2'
+    } else {
+      scores[5] = '1'
+    }
+  }
+
+  // 立ち幅とび
+  if (/^\d+(\.\d+)?$/.test(records.value[6])) {
+    const record = Number(records.value[6])
+    if (record >= { m: 265, f: 210 }[gender.value]) {
+      scores[6] = '10'
+    } else if (record >= { m: 254, f: 200 }[gender.value]) {
+      scores[6] = '9'
+    } else if (record >= { m: 242, f: 190 }[gender.value]) {
+      scores[6] = '8'
+    } else if (record >= { m: 230, f: 179 }[gender.value]) {
+      scores[6] = '7'
+    } else if (record >= { m: 218, f: 168 }[gender.value]) {
+      scores[6] = '6'
+    } else if (record >= { m: 203, f: 157 }[gender.value]) {
+      scores[6] = '5'
+    } else if (record >= { m: 188, f: 145 }[gender.value]) {
+      scores[6] = '4'
+    } else if (record >= { m: 170, f: 132 }[gender.value]) {
+      scores[6] = '3'
+    } else if (record >= { m: 150, f: 118 }[gender.value]) {
+      scores[6] = '2'
+    } else {
+      scores[6] = '1'
+    }
+  }
+
+  // ハンドボール投げ
+  if (/^\d+(\.\d+)?$/.test(records.value[7])) {
+    const record = Number(records.value[7])
+    if (record >= { m: 37, f: 23 }[gender.value]) {
+      scores[7] = '10'
+    } else if (record >= { m: 34, f: 20 }[gender.value]) {
+      scores[7] = '9'
+    } else if (record >= { m: 31, f: 18 }[gender.value]) {
+      scores[7] = '8'
+    } else if (record >= { m: 28, f: 16 }[gender.value]) {
+      scores[7] = '7'
+    } else if (record >= { m: 25, f: 14 }[gender.value]) {
+      scores[7] = '6'
+    } else if (record >= { m: 22, f: 12 }[gender.value]) {
+      scores[7] = '5'
+    } else if (record >= { m: 19, f: 11 }[gender.value]) {
+      scores[7] = '4'
+    } else if (record >= { m: 16, f: 10 }[gender.value]) {
+      scores[7] = '3'
+    } else if (record >= { m: 13, f: 8 }[gender.value]) {
+      scores[7] = '2'
+    } else {
+      scores[7] = '1'
+    }
+  }
+
+  // 総合点
+  if (/^\d+(\.\d+)?$/.test(records.value[8])) {
+    const record = Number(records.value[8])
+    if (record >= 65) {
+      scores[8] = 'A'
+    } else if (record >= 54) {
+      scores[8] = 'B'
+    } else if (record >= 43) {
+      scores[8] = 'C'
+    } else if (record >= 31) {
+      scores[8] = 'D'
+    } else {
+      scores[8] = 'E'
+    }
+  }
+  return scores
+}
+
+export const useRecordStore = defineStore('record', () => {
+  const gender = ref<'m'|'f'|null>(null)
+  const result = ref<Records>(['', '', '', '', '', '', '', '', ''])
+  const resultScores = computed(() => computeScores(gender, result))
+  const uecAve = ref<Records>(['', '', '', '', '', '', '', '', ''])
+  const uecAveScores = computed(() => computeScores(gender, uecAve))
+  const jpnAve = ref<Records>(['', '', '', '', '', '', '', '', ''])
+  const jpnAveScores = computed(() => computeScores(gender, jpnAve))
+  
+  return { gender, result, resultScores, uecAve, uecAveScores, jpnAve, jpnAveScores }
+})
